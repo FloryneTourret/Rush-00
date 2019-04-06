981791b40f5302872dee444ee46c7346983fe15f
Floryne <floryne.tourret.pro@gmail.com>
2019-04-06 18:48:07 +0200

    gestion des articles et des categories finies

--- views/admin/index_view.php
+++ views/admin/index_view.php
@@ -1,14 +1,11 @@
 <div class="col-6 offset-3 text-center">
-    <h1>admin</h1>
+    <h1>Administrateur</h1>
 </div>
 
 <div class="col-10 offset-1">
     <a href="ajout.php">Ajouter un article</a><br>
+    <a href="articles.php">Gérer les articles</a><br>
     <a href="categories.php">Gérer les catégories</a>
 </div>
-
-<p> -> ajouter un article / une catégorie </p>
-<p> -> modifier un article / une catégorie </p>
-<p> -> supprimer un article / une catégorie </p>
 <p> -> gestion des utilisateurs </p>
 <p> -> compte rendu des commandes </p>
