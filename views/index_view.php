acd9bb4bea81be3657145e09e3816bdd0959a60e
lnieto-101 <lnieto-101@github.com>
2019-04-07 16:21:58 +0200

    front

--- views/index_view.php
+++ views/index_view.php
@@ -1,14 +1,12 @@
 <div class="articles-content col-12">
-
-    <div class="col-6 offset-3 text-center">
-        <h1>ft_minishop</h1>
+    <div class="col-12">
+    
     </div>
-
-    <div class="articles-list offset-2">
+    <div class="articles-list offset-1">
         <?php 
             foreach($articles as $row)
             {
-                echo '<div class="col-4 article float-left article-'.$row['0'].'">';
+                echo '<div class="col-3 article float-left article-'.$row['0'].'">';
                 echo '<h2 class="article-title">'.$row['1'].'</h2>';
                 echo '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Emoji_u1f44c.svg/480px-Emoji_u1f44c.svg.png">';
                 echo '<p class="article-description">'.$row['2'].'</p>';
