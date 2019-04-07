e121c9b2478a6924a0dac1b38bb373d711b8a207
Frederic Leonard <fleonard@z4r7p3.le-101.fr>
2019-04-07 11:26:04 +0200

    changes

--- views/profil_view.php
+++ views/profil_view.php
@@ -1,6 +1,16 @@
-<div class="col-6 offset-3 text-center">
-    <h1>profil</h1>
+<div class="profile-content">
+    <div class="col-6 offset-3 text-center">
+        <h1>profil</h1>
+    </div>
+    <div class="delete-account">
+
+        <form action="delete_account.php" method="post">
+            <input type="password" name="pwd" id="pwd" placeholder="Entrez votre mot de passe" required><br>
+            <input class="delete-input" type="submit" value="Supprimer mon compte">
+        </form>
+
+    </div>
+    <p> -> supprimer son compte </p>
+    <p> -> modifier ses informations </p>
+    <p> -> Historique de ses commandes </p>
 </div>
-<p> -> supprimer son compte </p>
-<p> -> modifier ses informations </p>
-<p> -> Historique de ses commandes </p>
