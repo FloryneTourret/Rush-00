a08170bae7d1216689a57b62062fd27de3f47bdf
Floryne <floryne.tourret.pro@gmail.com>
2019-04-07 17:20:52 +0200

    commandes utilisateur

--- commandes.php
+++ commandes.php
@@ -1,15 +1,13 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['email']))
-    header('Location: ../index.php');
-
 include('configs/database.php');
-include('models/profil_model.php');
+include('models/commandes_model.php');
 
 include('views/base/header.php');
 include('views/base/sidebar_profil.php');
-include('views/profil_view.php');
+$commandes = get_commandes($mysqli);
+include('views/commandes_view.php');
 
 include('views/base/footer.php');
 ?>
---
+++ views/base/sidebar_profil.php
@@ -0,0 +1,17 @@
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="profil.php">Gérer mon profil</a><br>
+  <a href="commandes.php">Voir mon historique de commandes</a><br>
+</div>
+
+<span style="font-size:30px;cursor:pointer;margin-left:20px;margin-top:20px;" onclick="openNav()">&#9776;</span>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
