384aa3ae4f5ba6f83633d3ec70d8e9c5c6fe5435
Floryne <floryne.tourret.pro@gmail.com>
2019-04-07 20:48:45 +0200

    fix update article

--- admin/articles.php
+++ admin/articles.php
@@ -29,6 +29,18 @@ if(isset($_GET['update']))
         {
             $id = $_GET['update'];
             $desc = ucfirst(htmlspecialchars(addslashes($_GET['desc'])));
+
+            $caracteres = array('À' => 'a', 'Á' => 'a', 'Â' => 'a', 'Ä' => 'a', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ä' => 'a', '@' => 'a',
+            'È' => 'e', 'É' => 'e', 'Ê' => 'e', 'Ë' => 'e', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', '€' => 'e',
+            'Ì' => 'i', 'Í' => 'i', 'Î' => 'i', 'Ï' => 'i', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+            'Ò' => 'o', 'Ó' => 'o', 'Ô' => 'o', 'Ö' => 'o', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'ö' => 'o',
+            'Ù' => 'u', 'Ú' => 'u', 'Û' => 'u', 'Ü' => 'u', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'µ' => 'u',
+            'Œ' => 'oe', 'œ' => 'oe',
+            '$' => 's');
+            
+            $desc = strtr($desc, $caracteres);
+            $desc = preg_replace('#[^A-Za-z0-9]+#', ' ', $desc);
+            $desc = trim($desc);
             update_desc_article($mysqli, $id, $desc);
         }
     }
@@ -38,6 +50,19 @@ if(isset($_GET['update']))
         {
             $id = $_GET['update'];
             $titre = ucfirst(htmlspecialchars(addslashes($_GET['titre'])));
+
+            $caracteres = array('À' => 'a', 'Á' => 'a', 'Â' => 'a', 'Ä' => 'a', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ä' => 'a', '@' => 'a',
+            'È' => 'e', 'É' => 'e', 'Ê' => 'e', 'Ë' => 'e', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', '€' => 'e',
+            'Ì' => 'i', 'Í' => 'i', 'Î' => 'i', 'Ï' => 'i', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+            'Ò' => 'o', 'Ó' => 'o', 'Ô' => 'o', 'Ö' => 'o', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'ö' => 'o',
+            'Ù' => 'u', 'Ú' => 'u', 'Û' => 'u', 'Ü' => 'u', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'µ' => 'u',
+            'Œ' => 'oe', 'œ' => 'oe',
+            '$' => 's');
+            
+            $titre = strtr($titre, $caracteres);
+            $titre = preg_replace('#[^A-Za-z0-9]+#', ' ', $titre);
+            $titre = trim($titre);
+
             update_titre_article($mysqli, $id, $titre);
         }
     }
